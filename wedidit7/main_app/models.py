c5839cd63da11c487fff268e01ae26b2af13ae96
katcozadd <krosecozadd@gmail.com>
2018-05-27 16:09:41 -0600

    working on creating the data structure of sign up page

--- wedidit7/main_app/models.py
+++ wedidit7/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+first_name, last_name, phone, email, username, password, motto
+
+class User(models.Model):
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
+	phone = models.CharField(max_length=13)
+	email = models.CharField(max_length=100)
+	username = models.CharField(max_length=100)
+	password = models.CharField(max_length=100)
+	motto = models.CharField(max_length=1000)
