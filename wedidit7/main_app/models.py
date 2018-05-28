0b245fa8e0984554e507cabd02bdd331770bcc1f
katcozadd <krosecozadd@gmail.com>
2018-05-28 15:30:48 -0600

    working on fixing a post issue

--- wedidit7/main_app/models.py
+++ wedidit7/main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class User(models.Model):
@@ -9,6 +10,7 @@ class User(models.Model):
 	username = models.CharField(max_length=100)
 	password = models.CharField(max_length=100)
 	motto = models.CharField(max_length=1000)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 def __str__(self):
 	return self.name 
