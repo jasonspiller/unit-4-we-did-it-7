e097062e4f9a6756962a295f4de331183434851b
Jason Spiller <jason@thetargetlab.com>
2018-05-29 17:41:07 -0600

    Added controller/view.

--- main_app/views.py
+++ main_app/views.py
@@ -1,44 +1,56 @@
+"""Views."""
 from django.shortcuts import render, redirect
 # from django.contrib.auth import login, authenticate
 # from django.contrib.auth.decorators import login_required
 from .models import User, Share
 from django.http import HttpResponse, HttpResponseRedirect
-from .forms import SignUpForm
+from .forms import SignUpForm, ShareForm
 
-# Create your views here.
-
-# def index(request):
-# 	return render(request, 'index.html')
-
-# def sign_up(request):
-# 	user = User.objects.all()
-# 	form = SignUpForm()
-# 	return render(request, 'signup.html', {'user': user, 'form': form})
 
 def index(request):
-	user = User.objects.all()
-	form = SignUpForm()
-	return render(request, 'signup.html', {'user': user, 'form': form})
+    """Sign Up Form."""
+    user = User.objects.all()
+    form = SignUpForm()
+    return render(request, 'signup.html', {'user': user, 'form': form})
 
 # @login_required
 # def home(request):
 # 	return render(request, 'index.html')
 
+
 def show(request, user_id):
-	user = User.objects.get(id=user_id)
-	return render(request, 'profile.html', {'user': user})
+    """Show User."""
+    user = User.objects.get(id=user_id)
+    return render(request, 'profile.html', {'user': user})
+
 
 def signup(request):
-	form = SignUpForm(request.POST)
-	if form.is_valid():
-		user = User (
-			first_name = form.cleaned_data['first_name'],
-			last_name = form.cleaned_data['last_name'],
-			username = form.cleaned_data['username'],
-			email = form.cleaned_data['email'],
-			password = form.cleaned_data['password'],
-			phone = form.cleaned_data['phone'],
-			motto = form.cleaned_data['motto'])
-		user.user = request.user
-		user.save()
-	return HttpResponseRedirect('/')
+    """Sign Up Form."""
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        user = User(
+            first_name=form.cleaned_data['first_name'],
+            last_name=form.cleaned_data['last_name'],
+            username=form.cleaned_data['username'],
+            email=form.cleaned_data['email'],
+            password=form.cleaned_data['password'],
+            phone=form.cleaned_data['phone'],
+            motto=form.cleaned_data['motto'])
+        user.user = request.user
+        user.save()
+    return HttpResponseRedirect('/')
+
+
+def share(request):
+    """Share Form."""
+    form = ShareForm(request.POST)
+    if form.is_valid():
+        share = Share(
+            user_id=form.cleaned_data['share_date'],
+            story=form.cleaned_data['story'],
+            post_date=form.cleaned_data['post_date'],
+            media=form.cleaned_data['media'],
+            share_date=form.cleaned_data['share_date'])
+        share.share = request.share
+        share.save()
+    return HttpResponseRedirect('/')
