235964b645658b7f2ccf23c778226dc4f2ff6389
Jason Spiller <jason@thetargetlab.com>
2018-05-29 12:05:29 -0600

    Trying to connect deployed DB.

--- wedidit7/settings.py
+++ wedidit7/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,6 +85,9 @@ DATABASES = {
         'NAME': ('wedidit7'),
     }
 }
+
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
