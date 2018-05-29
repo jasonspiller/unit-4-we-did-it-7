65286c0f975b7a489420fd8f4868ae08a8879950
Jason Spiller <jason@thetargetlab.com>
2018-05-29 14:59:26 -0600

    static debugging

--- wedidit7/settings.py
+++ wedidit7/settings.py
@@ -125,7 +125,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
