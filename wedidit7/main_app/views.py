fc8ba9b72a3a7dc739a935d237b607a32c3d1fc3
katcozadd <krosecozadd@gmail.com>
2018-05-28 22:53:01 -0600

    finally figured out all of the user signup functionality and storing in database

--- wedidit7/main_app/views.py
+++ wedidit7/main_app/views.py
@@ -1,32 +1,47 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth import login, authenticate
+# from django.contrib.auth.decorators import login_required
 from .models import User
-from django.http import HttpResponseRedirect
-from main_app.forms import SignUpForm
+from django.http import HttpResponse, HttpResponseRedirect
+from .forms import SignUpForm
 
 # Create your views here.
 
-def index(request):
-	return render(request, 'index.html')
+# def index(request):
+# 	return render(request, 'index.html')
 
-@login_required
-def home(request):
-	return render(request, 'index.html')
+# def sign_up(request):
+# 	user = User.objects.all()
+# 	form = SignUpForm()
+# 	return render(request, 'signup.html', {'user': user, 'form': form})
+
+def index(request):
+	user = User.objects.all()
+	form = SignUpForm()
+	return render(request, 'signup.html', {'user': user, 'form': form})
+
+# @login_required
+# def home(request):
+# 	return render(request, 'index.html')
+
+def show(request, user_id):
+	user = User.objects.get(id=user_id)
+	return render(request, 'profile.html', {'user': user})
 
 def signup(request):
-	if request.method == 'POST':
-		form = SignUpForm(request.POST)
-		if form.is_valid():
-			username = form.cleaned_data.get('username')
-			raw_password = form.cleaned_data.get('password')
-			user = authenticate(username=username, password=raw_password)
-			form.save()
-			login(request, user)
-			return HttpResponseRedirect('home')
-	else: 
-		form = SignUpForm()
-	return render(request, 'signup.html', {'form': form})
+	form = SignUpForm(request.POST)
+	if form.is_valid():
+		user = User (
+			first_name = form.cleaned_data['first_name'],
+			last_name = form.cleaned_data['last_name'],
+			username = form.cleaned_data['username'],
+			email = form.cleaned_data['email'],
+			password = form.cleaned_data['password'],
+			phone = form.cleaned_data['phone'],
+			motto = form.cleaned_data['motto'])
+		user.user = request.user
+		user.save()
+	return HttpResponseRedirect('/')
 
 
 
