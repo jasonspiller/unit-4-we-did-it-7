5cfff2544f96cc3e65d682d529bd72938d9c9a85
katcozadd <krosecozadd@gmail.com>
2018-05-30 15:24:46 -0600

    sendgrid

--- wedidit7/settings.py
+++ wedidit7/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
 
 
 MIDDLEWARE = [
