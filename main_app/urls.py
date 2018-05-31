296608c1249179a85d9fc8750f4dc56889a3890d
Jason Spiller <jason@thetargetlab.com>
2018-05-31 10:29:58 -0600

    Majority of front-end in place.

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,18 +5,15 @@ from . import views
 
 
 urlpatterns = [
-<<<<<<< HEAD
-	# path('', views.index, name='index'),
-	# path('signup/', views.signup, name='signup'),
+    # path('', views.index, name='index'),
+    # path('signup/', views.signup, name='signup'),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', views.SignUp.as_view(), name='signup'),
-	path('<int:user_id>/', views.show, name='show'),
-	path('post_url/', views.signup, name='signup'),
-	path('profile/', views.profile, name='profile'),
-	path('storyline/', views.storylineview, name='Storyline'),
-	path('post/',views.post, name='post'),
-	path('signin/', views.signin, name='signin'),
-] 
-
-
+    # path('<int:user_id>/', views.show, name='show'),
+    # path('post_url/', views.signup, name='signup'),
+    path('profile/', views.profile, name='profile'),
+    path('storyline/', views.storyline, name='storyline'),
+    path('share/', views.share, name='share'),
+    path('signin/', views.signin, name='signin'),
+]
--- main_app/views.py
+++ main_app/views.py
@@ -4,33 +4,41 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from .models import User, Share
+#from .models import User, Share
 from django.views import generic
 
 
 class SignUp(generic.CreateView):
     """Sign Up Form."""
-	form_class = UserCreationForm
+
+    form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'	
+    template_name = 'signup.html'
 
 
-def index(request):
-	user = User.objects.all()
-	form = SignUpForm()
-	return render(request, 'index.html')
+# def index(request):
+# 	user = User.objects.all()
+# 	form = SignUpForm()
+# 	return render(request, 'index.html')
 
-def storylineview(request):
-    return render(request, 'storyline.html',{})
+def storyline(request):
+    """Storyline."""
+    return render(request, 'storyline.html', {})
+
 
 def profile(request):
-	return render(request, 'profile.html',{})
+    """Profile."""
+    return render(request, 'profile.html', {})
 
-def post(request):
-	return render(request, 'post.html',{})
+
+def share(request):
+    """Shareself."""
+    return render(request, 'share.html', {})
+
 
 def signin(request):
-	return render(request, 'signin.html',{})
+    """Sign In."""
+    return render(request, 'signin.html', {})
 
 
 # def post_share(request):
