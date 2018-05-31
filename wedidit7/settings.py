59d2b9a566fde7377f333f2ea117fd28cb0ee127
katcozadd <krosecozadd@gmail.com>
2018-05-31 11:18:49 -0600

    pycach

--- wedidit7/settings.py
+++ wedidit7/settings.py
@@ -144,16 +144,16 @@ LOGOUT_REDIRECT_URL = 'home'
 
 # using SendGrid's Python Library
 # https://github.com/sendgrid/sendgrid-python
-import sendgrid
-from sendgrid.helpers.mail import *
+# import sendgrid
+# from sendgrid.helpers.mail import *
 
-sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
-from_email = Email("test@example.com")
-to_email = Email("krosecozadd@gmail.com")
-subject = "Sending with SendGrid is Fun"
-content = Content("text/plain", "please freaking work I don't want to cry anymore")
-mail = Mail(from_email, subject, to_email, content)
-response = sg.client.mail.send.post(request_body=mail.get())
-print(response.status_code)
-print(response.body)
-print(response.headers)
+# sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+# from_email = Email("test@example.com")
+# to_email = Email("krosecozadd@gmail.com")
+# subject = "Sending with SendGrid is Fun"
+# content = Content("text/plain", "please freaking work I don't want to cry anymore")
+# mail = Mail(from_email, subject, to_email, content)
+# response = sg.client.mail.send.post(request_body=mail.get())
+# print(response.status_code)
+# print(response.body)
+# print(response.headers)
